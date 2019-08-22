ae6fe6546cef3910bafd36d663a549cc0735bc8b
Chris Park <junkin@berkeley.edu>
2019-08-21 17:06:17 -0700

    Javascript fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,12 @@
+// jQuery
 $(document).ready(function(){
-	// Navbar changes color when you scroll 
-   $(window).scroll(function(){
+	// Navbar page indicator
+	if ($('#is-lessons').length > 0) {
+		$('#lessons-nav').css('color', 'grey')
+	}
+
+	/* Navigation bar will change color after scrolling */
+   $(window).scroll(function() {
   		var scroll = $(window).scrollTop();
 	  	if (scroll > 0) {
 	    	$(".navbar").css("background" , "#FFFFFF");
@@ -13,62 +19,70 @@ $(document).ready(function(){
 		 	$(".navbar").css("box-shadow" , "0px 0px 0px");	
 	  	}
   	})
+
+   	/* Increases homepage main image size and decreases its opacity when scrolling down */
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		$(".main-pic").css('opacity', 1 - scroll/5500)
+		$(".main-pic").css("background-size", (100 + scroll/70) + "%");
+	});
    		
-	// Navbar Redirect and Scrolling 
-	if ($('body.is-home').length > 0) {
+	/* Navigation bar functions - 
+	Scrolls to section on homepage if on homepage. 
+	Goes back to homepage if not on homepage. */
+	$("#home-nav").click(function() {
+		if ($('#is-home').length > 0) {
+			$('html, body').animate({scrollTop: $(".main-pic").position().top}, 650);
+		} else {
+			window.location.href='/';
+		}
+	});
+	
+	$("#about-us-nav").click(function() {
+		if ($('#is-home').length > 0) {
+			$('html, body').animate({scrollTop: $('#about-us').offset().top - 28}, 650);
+		} else {
+			window.location.href='/';
+		}  
+	});
 
-		// Scrolling
+	$("#projects-nav").click(function() {
+		if ($('#is-home').length > 0) {
+			$('html, body').animate({scrollTop: $('#project-box').offset().top - 0}, 650);
+		} else {
+			window.location.href='/';
+		}
+	});
 
-		$("#home-nav").click(function() {
-	    	$('html, body').animate({scrollTop: $(".main-pic").position().top}, 650);
-		});
-		$("#about-us-nav").click(function() {
-	    	$('html, body').unbind().animate({scrollTop: $('#about-us').offset().top - 28}, 650);
-		});
-		$("#projects-nav").click(function() {
-		    $('html, body').unbind().animate({scrollTop: $('#project-box').offset().top - 0}, 650);
-		});
-		$("#contact-nav").click(function() {
-		    $('html, body').animate({scrollTop: $("#contact").position().top - 40}, 650);
-		});
+	$("#contact-nav").click(function() {
+		if ($('#is-home').length > 0) {
+			$('html, body').animate({scrollTop: $("#contact").position().top - 40}, 650);
+		} else {
+			window.location.href='/';
+		}
+	});
 
-		// Redirect
+	$('#about-us-more').click(function() {
+   		window.location.href='https://docs.google.com/presentation/d/1YP8t1x-uY0AQk9Xtad_j2C6JiEsX-FJ5bvYuSMVNBIQ/edit?usp=sharing';
+	});
 
-		$('#about-us-more').click(function() {
-	   		window.location.href='https://docs.google.com/presentation/d/1YP8t1x-uY0AQk9Xtad_j2C6JiEsX-FJ5bvYuSMVNBIQ/edit?usp=sharing';
-		});
-
-	} else {
-		$('#home-nav').click(function() {
-   			window.location.href='/';
-		});
-		$('#about-us-nav').click(function() {
-	   		window.location.href='/';
-		});
-		$('#projects-nav').click(function() {
-	   		window.location.href='/';
-		});
-		$('#contact-nav').click(function() {
-	   		window.location.href='https://docs.google.com/forms/d/e/1FAIpQLSe4U1fDkjBbr7y-ynNHrlp5B8n6Ai7_XrlOg965Q3lifYUV1w/viewform?usp=sf_link';
-		});
-	}
 	$('#apply-link').click(function() {
 	   	window.location.href='https://docs.google.com/forms/d/e/1FAIpQLSc-2SlPRjY6Fh6QzTlxo3xJcVIkfEkLQK4xkGGDiCLRrk_oow/viewform?usp=sf_link';
 		});
+	
 	$('#lessons-nav').click(function() {
-	   	window.location.href='/lessons';
+	   	window.location.href='lessons.html'; // "/lessons"
 		});
 	/* .htaccess checks if the specified directory exists and then rewrites the name 
 		https://stackoverflow.com/questions/5730092/how-to-remove-html-from-url
 		*/
 
-	// Footer Buttons
-
+	// Footer button functions
 	$('#footer-about').click(function() {
 	   	window.location.href='/';
 		});
 	$('#footer-project').click(function() {
-	   		window.location.href='/';
+	   	window.location.href='/';
 		});
 	$('#footer-team').click(function() {
 	   	window.location.href='/';
@@ -80,11 +94,11 @@ $(document).ready(function(){
 	   	window.location.href='https://docs.google.com/forms/d/e/1FAIpQLSe4U1fDkjBbr7y-ynNHrlp5B8n6Ai7_XrlOg965Q3lifYUV1w/viewform?usp=sf_link';
 		});
 
-	// Dropdown
-
-	var d = $('.lessons-drop-down')
+	/* Unused - Hides or shows objects when a dropdown button is pressed */
 	$('.dropdown-button').click(function() {
+		var d = $('.lessons-drop-down')
 		$('.dropdown-button').css('transform', 'rotate(180deg)');
+		
 		if (d.hasClass('is-hidden')) {
 		   	d.css('display', 'block').removeClass('is-hidden');
 		   	$('.dropdown-button').css('transform', 'rotate(0deg)');
@@ -92,29 +106,4 @@ $(document).ready(function(){
 		   	d.css('display', 'none').addClass('is-hidden');
 		}
 	});
-	
-	// Page Indicator
-
-	if ($('#is-lessons').length > 0) {
-		$('#lessons-nav').css('color', 'grey')
-	}
-	if ($('#is-contact').length > 0) {
-		$('#contact-nav').css('color', 'grey')
-	}
-
-	// Main Image Effect
-
-	$(window).scroll(function(){
-		var scroll = $(window).scrollTop();
-		$(".main-pic").css('opacity', 1 - scroll/5500)
-		$(".main-pic").css("background-size", (100 + scroll/70) + "%");
-	});
-
-
-	// $(window).scroll(function(){
- //  		var scroll = $(window).scrollTop();
-	// 	$("#computer-pic, #main-desc-box, .main-pic").css({top: -19 -(scroll/25)  + "%"});
- //  		$("#main-desc-box").css({top: 31 -(scroll/25)  + "%"});
- //  		// $(".main-pic").css("box-shadow" , "inset -2px 0px 5px 2px black");	
- //  	});
 });
